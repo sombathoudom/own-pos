d4978defec974ca110080405b1f3c0f73eea79ed
Oudom <sombathoudom001@gmail.com>
2026-05-20 01:39:11 +0700

    hide font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,7 +34,7 @@
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-        @fonts
+        <!-- @fonts -->
 
         @viteReactRefresh
         @vite(['resources/js/app.tsx'])
